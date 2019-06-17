86a56550ce3f4ff8504c513101fe6c104cc9d518
murtuswami <harsharamachandran14@gmail.com>
2019-06-17 15:05:37 +0800

    added some func

--- src/App.js
+++ src/App.js
@@ -2,56 +2,179 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { sequenceExpression } from '@babel/types';
-import audio1 from './beep-01a.mp3'
 
 class App extends React.Component {
  constructor(props){
    super(props);
-   this.audio1 = React.createRef();
-   this.playAudio = this.playAudio.bind(this);
- }
- playAudio(event){
-  
-  switch(event.target.id){
-    case "1": 
-    this.audio1.play()
-    break;
-   default:
-     console.log("def")
-     break;
- }
+    const bankOne = [{
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  }, {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  }, {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  }, {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  }, {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  }, {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  }, {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  }, {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  }, {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  },
+];
+    const bankTwo = [{
+  keyCode: 81,
+  keyTrigger: 'Q',
+  id: 'Chord-1',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+}, {
+  keyCode: 87,
+  keyTrigger: 'W',
+  id: 'Chord-2',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+}, {
+  keyCode: 69,
+  keyTrigger: 'E',
+  id: 'Chord-3',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+}, {
+  keyCode: 65,
+  keyTrigger: 'A',
+  id: 'Shaker',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+}, {
+  keyCode: 83,
+  keyTrigger: 'S',
+  id: 'Open-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+}, {
+  keyCode: 68,
+  keyTrigger: 'D',
+  id: 'Closed-HH',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+}, {
+  keyCode: 90,
+  keyTrigger: 'Z',
+  id: 'Punchy-Kick',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+}, {
+  keyCode: 88,
+  keyTrigger: 'X',
+  id: 'Side-Stick',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+}, {
+  keyCode: 67,
+  keyTrigger: 'C',
+  id: 'Snare',
+  url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+}];
+this.state = {currentPadBank:bankOne}
+
+
 }
+
  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <div id = "drum-machine"> 
-            <div id ="display"></div>
-            <div id ="pads">
-              <div id ="row">
-                <button className="drum-pad" id="1" onClick={this.playAudio}><audio id="clip" id="Q" ref ={(audio1) => { this.audio1 = audio1;}} src= {audio1}></audio>Q</button>
-                <button className="drum-pad"><audio id="clip" id="W" ref ={(audio2) => { this.audio2 = audio2;}}></audio>W</button>
-                <button className="drum-pad"><audio id="clip" id="E" ref ={(audio3) => { this.audio3 = audio3;}}></audio>E</button>
-              </div>
-              <div id ="row">
-                <button className="drum-pad"><audio id="clip" id="A"ref ={(audio4) => { this.audio4 = audio4;}} ></audio>A</button>
-                <button className="drum-pad"><audio id="clip" id="S"ref ={(audio5) => { this.audio5 = audio5;}} ></audio>S</button>
-                <button className="drum-pad"><audio id="clip" id="D"ref ={(audio6) => { this.audio6 = audio6;}} ></audio>D</button>
-              </div>
-              <div id ="row">
-                <button className="drum-pad"><audio id="clip" id="Z"ref ={(audio7) => { this.audio7 = audio7;}}></audio>Z</button>
-                <button className="drum-pad"><audio id="clip" id="X"ref ={(audio8) => { this.audio8= audio8;}}></audio>X</button>
-                <button className="drum-pad"><audio id="clip" id="C"ref ={(audio9) => { this.audio9 = audio9;}}></audio>C</button>
-              </div>
-
-            </div>
-        
-        </div>
-
+      <header id ="drum-machine">
+      <PadBoard currentPadBank = {this.state.currentPadBank}/>
+      <div id = "display">Nothing</div>
       </header>
     </div>
   );
 }
 }
 
+class DrumPad extends React.Component{
+  constructor(props){
+    super(props);
+    this.playAudio = this.playAudio.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.keydown = props.keydown;
+ 
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+  handleKeyPress(e){
+    console.log(e.keyCode)
+    console.log(this.keydown)
+   
+    if(e.keyCode == this.props.keydown)
+    {
+    this.playAudio()
+   
+    }
+  }
+  playAudio(){
+    document.getElementById("display").innerHTML = this.props.name;
+    let sound = document.getElementById(this.props.keyTrigger);
+    console.log(this.props);
+    sound.play();
+   }  
+  
+  render(){ 
+    return  (<button className ="drum-pad"  onClick = {this.playAudio}>
+      <audio className ="clip" id = {this.props.keyTrigger} src ={this.props.url}></audio>
+      {this.props.keyTrigger}
+      </button>);
+  }
+}
+
+
+class PadBoard extends React.Component {
+constructor(props){
+   super(props);
+   
+  }
+  render(){
+ 
+    let padBank = this.props.currentPadBank.map((obj,i,arr) => {
+      return (
+      <DrumPad 
+      keyTrigger = {arr[i].keyTrigger} keydown = {arr[i].keyCode} url = {arr[i].url} name = {arr[i].id}  />
+      );
+    });
+   
+    return (
+      <div>
+    {padBank}
+    </div>
+    );
+  }
+}
 export default App;
